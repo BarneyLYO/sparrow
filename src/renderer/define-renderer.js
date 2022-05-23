f865d0e82f55d1bbe6c0922f202cf7fdf7b37a93
BarneyLYO <barneyliu@hotmail.com>
2022-05-24 02:21:03 +0800

    chore: redesign

---
+++ src/renderer/define-renderer.js
@@ -0,0 +1,53 @@
+const SUPPORTED_SHAPE_IMPLES = [
+  'line',
+  'circle',
+  'text',
+  'rect',
+  'path',
+  'ring',
+]
+
+const SUPPORTED_TRANSFORM_IMPLES = [
+  'restore',
+  'save',
+  'scale',
+  'translate',
+  'rotate',
+]
+
+export function defineRenderer(
+  ctxImpl,
+  shapesImpl,
+  transformImpl,
+) {
+  const createRenderer = (w, h) => {
+    const ctx = ctxImpl(w, h)
+
+    const shapesFns = SUPPORTED_SHAPE_IMPLES.reduce(
+      (accu, curr) => ({
+        ...accu,
+        [curr]: (opts) => shapesImpl[curr](ctx, opts),
+      }),
+      Object.create(null),
+    )
+
+    const transformFns = SUPPORTED_TRANSFORM_IMPLES.reduce(
+      (accu, curr) => ({
+        ...accu,
+        [curr]: (...args) =>
+          // eslint-disable-next-line implicit-arrow-linebreak
+          transformImpl[curr](ctx, ...args),
+      }),
+      Object.create(null),
+    )
+
+    return {
+      node: () => ctx.node,
+      group: () => ctx.group,
+      ...shapesFns,
+      ...transformFns,
+    }
+  }
+
+  return createRenderer
+}
--- src/renderer/roughjs/context.js
+++
@@ -1,7 +0,0 @@
-import rc from 'roughjs'
-
-export function intensifyContext(ctx) {
-  const { node } = ctx
-  const rough = rc.svg(node)
-  return { ...ctx, rough }
-}
